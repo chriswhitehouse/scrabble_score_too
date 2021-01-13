a893971dd802d0b049681272a13f2a4bc8e7a73e
Chris Whitehouse <chris.whitehouse@hotmail.com>
2021-01-13 17:12:49 +0000

    Passing third unit test

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,13 +1,24 @@
 class Scrabble
   def initialize(word)
     @word = word
+
+    @letter_scores = {
+      1 => ['A', 'S', 'T', 'R', 'E'],
+      4 => ['F']
+    }
+
+    @score = 0
   end
 
   def score
-    if @word == 'a'
-      1
-    elsif @word == 'f'
-      4
+    @word.split('').each do |letter|
+      @letter_scores.each do |letter_score, letter_array|
+        if letter_array.include?(letter.upcase)
+          @score += letter_score
+        end
+      end
     end
+
+    return @score
   end
 end
