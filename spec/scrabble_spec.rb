e26f4b3ff57b9c2022d475334271a38780ed8225
Chris Whitehouse <chris.whitehouse@hotmail.com>
2021-01-13 17:04:52 +0000

    Failing third unit test

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -11,5 +11,10 @@ describe Scrabble do
       scrabble = Scrabble.new('f')
       expect(scrabble.score).to eq(4)
     end
+
+    it "should return 6 when passed street" do
+      scrabble = Scrabble.new('street')
+      expect(scrabble.score).to eq(6)
+    end
   end
 end
