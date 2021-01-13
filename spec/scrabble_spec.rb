43e8a8a375c2772d471eb51af2b76c4090b00f48
Chris Whitehouse <chris.whitehouse@hotmail.com>
2021-01-13 17:02:31 +0000

    Failing second unit test

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -6,5 +6,10 @@ describe Scrabble do
       scrabble = Scrabble.new('a')
       expect(scrabble.score).to eq(1)
     end
+
+    it "should return 4 when passed f" do
+      scrabble = Scrabble.new('f')
+      expect(scrabble.score).to eq(4)
+    end
   end
 end
